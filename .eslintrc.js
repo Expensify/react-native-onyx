e01c24337da0b46579049fd3b567da4e1d6321fe
Tim Golen <tgolen@expensify.com>
2020-11-11 12:40:15 -0700

    Fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
     extends: 'expensify',
+    rules: {
+        // Overwriting this for now because web-e will conflict with this
+        'react/jsx-filename-extension': [1, {extensions: ['.js']}],
+    },
 };
