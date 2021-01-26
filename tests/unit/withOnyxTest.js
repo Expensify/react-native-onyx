38fde3d4967e9108bb6fb27b2c967d768f6b7e52
chiragsalian <chirag@expensify.com>
2021-01-25 17:52:05 -0800

    corrections and tests

--- tests/unit/withOnyxTest.js
+++ tests/unit/withOnyxTest.js
@@ -42,74 +42,42 @@ describe('withOnyx', () => {
     });
 });
 
-// describe('withOnyx', () => {
-//     it('should update withOnyx subscriber just once when mergeCollection is used', () => {
-//         const logSpy = jest.spyOn(console, 'log');
-//         const TestComponentWithOnyx = withOnyx({
-//             text: {
-//                 key: ONYX_KEYS.COLLECTION.TEST_KEY,
-//             },
-//         })(ViewWithCollections);
-//         const result = render(<TestComponentWithOnyx />);
-
-//         // Onyx.mergeCollection(ONYX_KEYS.COLLECTION.TEST_KEY, {test_1: {ID: 123}, test_2: {ID: 234}, test_3: {ID: 345}});
-//         Onyx.merge(ONYX_KEYS.COLLECTION.TEST_KEY+'1',{ID: 123});
-//         Onyx.merge(ONYX_KEYS.COLLECTION.TEST_KEY+'2',{ID: 234});
-//         Onyx.merge(ONYX_KEYS.COLLECTION.TEST_KEY+'3',{ID: 345});
-//         return waitForPromisesToResolve()
-//             .then(() => {
-//                 expect(logSpy).toHaveBeenCalledTimes(4);
-//             });
-//     });
-// });
-
 describe('withOnyx', () => {
-    it('should update withOnyx subscriber just once when mergeCollection is used', () => {
-        const logSpy2 = jest.spyOn(console, 'log');
+    it('should update withOnyx subscriber multiple times when merge is used', () => {
+        const logSpy = jest.spyOn(console, 'log');
         const TestComponentWithOnyx = withOnyx({
             text: {
                 key: ONYX_KEYS.COLLECTION.TEST_KEY,
             },
         })(ViewWithCollections);
-        const result = render(<TestComponentWithOnyx />);
+        render(<TestComponentWithOnyx />);
+
+        Onyx.merge(ONYX_KEYS.COLLECTION.TEST_KEY + '1', {ID: 123});
+        Onyx.merge(ONYX_KEYS.COLLECTION.TEST_KEY + '2', {ID: 234});
+        Onyx.merge(ONYX_KEYS.COLLECTION.TEST_KEY + '3', {ID: 345});
+        return waitForPromisesToResolve()
+            .then(() => {
+                expect(logSpy).toHaveBeenCalledTimes(4);
+                console.log.mockClear();
+            });
+    });
+});
+
+describe('withOnyx', () => {
+    it('should update withOnyx subscriber just once when mergeCollection is used', () => {
+        const logSpy = jest.spyOn(console, 'log');
+        const TestComponentWithOnyx = withOnyx({
+            text: {
+                key: ONYX_KEYS.COLLECTION.TEST_KEY,
+            },
+        })(ViewWithCollections);
+        render(<TestComponentWithOnyx />);
 
         Onyx.mergeCollection(ONYX_KEYS.COLLECTION.TEST_KEY, {test_1: {ID: 123}, test_2: {ID: 234}, test_3: {ID: 345}});
         return waitForPromisesToResolve()
             .then(() => {
-                expect(logSpy2).toHaveBeenCalledTimes(1);
+                expect(logSpy).toHaveBeenCalledTimes(2);
+                console.log.mockClear();
             });
-    });
-});
-
-describe('withOnyx', () => {
-    it('should receive callback for each object change for mergeCollection', () => {
-        const valuesReceived = [];
-        let numberOfCallbacks = 0;
-        Onyx.connect({
-            key: ONYX_KEYS.COLLECTION.TEST_KEY,
-            initWithStoredValues: false,
-            callback: (value) => {
-                ++numberOfCallbacks;
-                valuesReceived.push(value);
-            },
-        });
-
-        Onyx.mergeCollection(ONYX_KEYS.COLLECTION.TEST_KEY, {test_1: {ID: 123}, test_2: {ID: 234}, test_3: {ID: 345}})
-            .then(() => {
-                expect(numberOfCallbacks).toEqual(3);
-                expect(valuesReceived[0]).toEqual({ID: 123});
-                expect(valuesReceived[1]).toEqual({ID: 234});
-                expect(valuesReceived[2]).toEqual({ID: 345});
-            });
-    });
-});
-
-describe('withOnyx', () => {
-    it('should throw error when a key not belonging to collection key is present in mergeCollection', () => {
-        try {
-            Onyx.mergeCollection(ONYX_KEYS.COLLECTION.TEST_KEY, {test_1: {ID: 123}, not_my_test: {beep: 'boop'}})
-        } catch (error) {
-            expect(error.message).toEqual(`Provided collection does not have all its data belonging to the same parent. CollectionKey: ${ONYX_KEYS.COLLECTION.TEST_KEY}, DataKey: not_my_test`);
-        }
     });
 });
