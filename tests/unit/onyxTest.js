173f688e74bb5e44e0b0c02ed54bfef6c7315e28
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-04-02 12:20:35 -1000

    remove initWithStoredValues

--- tests/unit/onyxTest.js
+++ tests/unit/onyxTest.js
@@ -26,7 +26,6 @@ describe('Onyx', () => {
 
         connectionID = Onyx.connect({
             key: ONYX_KEYS.TEST_KEY,
-            initWithStoredValues: false,
             callback: (value) => {
                 testKeyValue = value;
             },
@@ -44,7 +43,6 @@ describe('Onyx', () => {
 
         connectionID = Onyx.connect({
             key: ONYX_KEYS.TEST_KEY,
-            initWithStoredValues: false,
             callback: (value) => {
                 testKeyValue = value;
             },
@@ -65,7 +63,6 @@ describe('Onyx', () => {
         let testKeyValue;
         connectionID = Onyx.connect({
             key: ONYX_KEYS.TEST_KEY,
-            initWithStoredValues: false,
             callback: (value) => {
                 testKeyValue = value;
             }
@@ -85,7 +82,6 @@ describe('Onyx', () => {
         let testKeyValue;
         connectionID = Onyx.connect({
             key: ONYX_KEYS.TEST_KEY,
-            initWithStoredValues: false,
             callback: (value) => {
                 testKeyValue = value;
             },
@@ -107,7 +103,6 @@ describe('Onyx', () => {
         let testKeyValue;
         connectionID = Onyx.connect({
             key: ONYX_KEYS.TEST_KEY,
-            initWithStoredValues: false,
             callback: (value) => {
                 testKeyValue = value;
             },
@@ -128,7 +123,6 @@ describe('Onyx', () => {
         let testKeyValue;
         connectionID = Onyx.connect({
             key: ONYX_KEYS.TEST_KEY,
-            initWithStoredValues: false,
             callback: (value) => {
                 testKeyValue = value;
             },
@@ -146,7 +140,6 @@ describe('Onyx', () => {
         let testKeyValue;
         connectionID = Onyx.connect({
             key: ONYX_KEYS.TEST_KEY,
-            initWithStoredValues: false,
             callback: (value) => {
                 testKeyValue = value;
             },
@@ -165,7 +158,6 @@ describe('Onyx', () => {
         const mockCallback = jest.fn(data => valuesReceived[data.ID] = data.value);
         connectionID = Onyx.connect({
             key: ONYX_KEYS.COLLECTION.TEST_KEY,
-            initWithStoredValues: false,
             callback: mockCallback,
         });
 
@@ -209,28 +201,31 @@ describe('Onyx', () => {
             ))
             .then(() => {
                 // 3 items on the first mergeCollection + 4 items the next mergeCollection
-                expect(mockCallback.mock.calls.length).toBe(7);
+                expect(mockCallback.mock.calls.length).toBe(8);
 
-                expect(mockCallback.mock.calls[0][0]).toEqual({ID: 123, value: 'one'});
-                expect(mockCallback.mock.calls[0][1]).toEqual('test_1');
+                expect(mockCallback.mock.calls[0][0]).toEqual(null);
+                expect(mockCallback.mock.calls[0][1]).toEqual('test_');
 
-                expect(mockCallback.mock.calls[1][0]).toEqual({ID: 234, value: 'two'});
-                expect(mockCallback.mock.calls[1][1]).toEqual('test_2');
+                expect(mockCallback.mock.calls[1][0]).toEqual({ID: 123, value: 'one'});
+                expect(mockCallback.mock.calls[1][1]).toEqual('test_1');
 
-                expect(mockCallback.mock.calls[2][0]).toEqual({ID: 345, value: 'three'});
-                expect(mockCallback.mock.calls[2][1]).toEqual('test_3');
+                expect(mockCallback.mock.calls[2][0]).toEqual({ID: 234, value: 'two'});
+                expect(mockCallback.mock.calls[2][1]).toEqual('test_2');
 
-                expect(mockCallback.mock.calls[3][0]).toEqual({ID: 123, value: 'five'});
-                expect(mockCallback.mock.calls[3][1]).toEqual('test_1');
+                expect(mockCallback.mock.calls[3][0]).toEqual({ID: 345, value: 'three'});
+                expect(mockCallback.mock.calls[3][1]).toEqual('test_3');
 
-                expect(mockCallback.mock.calls[4][0]).toEqual({ID: 234, value: 'four'});
-                expect(mockCallback.mock.calls[4][1]).toEqual('test_2');
+                expect(mockCallback.mock.calls[4][0]).toEqual({ID: 123, value: 'five'});
+                expect(mockCallback.mock.calls[4][1]).toEqual('test_1');
 
-                expect(mockCallback.mock.calls[5][0]).toEqual({ID: 456, value: 'two'});
-                expect(mockCallback.mock.calls[5][1]).toEqual('test_4');
+                expect(mockCallback.mock.calls[5][0]).toEqual({ID: 234, value: 'four'});
+                expect(mockCallback.mock.calls[5][1]).toEqual('test_2');
 
-                expect(mockCallback.mock.calls[6][0]).toEqual({ID: 567, value: 'one'});
-                expect(mockCallback.mock.calls[6][1]).toEqual('test_5');
+                expect(mockCallback.mock.calls[6][0]).toEqual({ID: 456, value: 'two'});
+                expect(mockCallback.mock.calls[6][1]).toEqual('test_4');
+
+                expect(mockCallback.mock.calls[7][0]).toEqual({ID: 567, value: 'one'});
+                expect(mockCallback.mock.calls[7][1]).toEqual('test_5');
 
                 expect(valuesReceived[123]).toEqual('five');
                 expect(valuesReceived[234]).toEqual('four');
