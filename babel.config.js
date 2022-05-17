79881fdcda7888941789b5659da7a53adaa3f6a6
Peter Velkov <kidroca@gmail.com>
2022-05-17 10:03:54 +0300

    Remove unnecessary babel presets and packages
    These presets are already covered by `metro-react-native-babel-preset`
    We should either use the metro preset or compose react/env/flow ourselves but not both

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     presets: [
         require('metro-react-native-babel-preset'),
-        '@babel/preset-react',
-        '@babel/preset-env',
-        '@babel/preset-flow',
     ],
     plugins: [
         '@babel/plugin-proposal-class-properties',
