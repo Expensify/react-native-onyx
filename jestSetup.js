f302205bb515d02093b13dc2370f727dd37fa84f
Christoph Pader <christoph@margelo.io>
2023-03-14 17:33:08 -0400

    mock LocalForage storage provider with storage mock

--- jestSetup.js
+++ jestSetup.js
@@ -1,2 +1,3 @@
 jest.mock('./lib/storage/NativeStorage', () => require('./lib/storage/__mocks__'));
 jest.mock('./lib/storage/WebStorage', () => require('./lib/storage/__mocks__'));
+jest.mock('./lib/storage/providers/LocalForage', () => require('./lib/storage/__mocks__'));
