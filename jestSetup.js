eefc83a1cfc943863bb88c11345cf53e1f6c7bf0
Christoph Pader <christoph@margelo.io>
2023-04-12 22:52:15 +0200

    update comment

--- jestSetup.js
+++ jestSetup.js
@@ -4,6 +4,6 @@ jest.mock('./lib/storage/WebStorage', () => require('./lib/storage/__mocks__'));
 jest.mock('./lib/storage/providers/LocalForage', () => require('./lib/storage/__mocks__'));
 
 // Jest doesn't recognize native file extensions such as `.web.js` or `.native.js`.
-// This is why we need to default to the web implementation for tests
+// This is why we need to default one of the platforms implementations
 jest.mock('./lib/OnyxMerge', () => require('./lib/OnyxMerge.web'));
 jest.mock('./lib/metrics', () => require('./lib/metrics/index.native'));
