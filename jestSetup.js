02e1e880c3e9fa2750d3d6642bf843624b433a37
Christoph Pader <christoph@margelo.io>
2023-04-28 15:07:36 +0200

    remove OnyxMerge files and simplify merge logic

--- jestSetup.js
+++ jestSetup.js
@@ -2,7 +2,3 @@ jest.mock('./lib/storage');
 jest.mock('./lib/storage/NativeStorage', () => require('./lib/storage/__mocks__'));
 jest.mock('./lib/storage/WebStorage', () => require('./lib/storage/__mocks__'));
 jest.mock('./lib/storage/providers/LocalForage', () => require('./lib/storage/__mocks__'));
-
-// Jest doesn't recognize native file extensions such as `.web.js` or `.native.js`.
-// This is why we need to default one of the platforms implementations
-jest.mock('./lib/OnyxMerge', () => require('./lib/OnyxMerge.web'));
