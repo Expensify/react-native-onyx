870d7158aa2b2045be15a0f324a38a1385355ff7
Ana Margarida Silva <ana.margarida.silva@callstack.com>
2023-09-04 14:52:22 +0100

    feat: broadcast

---
+++ lib/broadcast/index.d.ts
@@ -0,0 +1,22 @@
+/**
+ * Sends a message to the broadcast channel.
+ */
+declare function sendMessage(message: string): void;
+
+/**
+ * Subscribes to the broadcast channel. Every time a new message
+ * is received, the callback is called.
+ */
+declare function subscribe(callback: () => {}): void;
+
+/**
+ * Unsubscribes from the broadcast channel.
+ */
+declare function unsubscribe(callback: () => {}): void;
+
+/**
+ * Disconnects from the broadcast channel.
+ */
+declare function disconnect(): void;
+
+export {sendMessage, subscribe, unsubscribe, disconnect};
