b86d500cff3c39bda5c6444f5bdc6d269109691d
Blazej Kustra <blazej.kustra@swmansion.com>
2023-09-26 14:04:03 +0200

    Update types for 'Remove null values in nested objects'

--- lib/Onyx.d.ts
+++ lib/Onyx.d.ts
@@ -1,7 +1,6 @@
 import {Component} from 'react';
-import {PartialDeep} from 'type-fest';
 import * as Logger from './Logger';
-import {CollectionKey, CollectionKeyBase, DeepRecord, KeyValueMapping, OnyxCollection, OnyxEntry, OnyxKey, NullableProperties} from './types';
+import {CollectionKey, CollectionKeyBase, DeepRecord, KeyValueMapping, NullishDeep, OnyxCollection, OnyxEntry, OnyxKey} from './types';
 
 /**
  * Represents a mapping object where each `OnyxKey` maps to either a value of its corresponding type in `KeyValueMapping` or `null`.
@@ -79,14 +78,14 @@ type OnyxUpdate =
               | {
                     onyxMethod: typeof METHOD.MERGE;
                     key: TKey;
-                    value: PartialDeep<KeyValueMapping[TKey]>;
+                    value: NullishDeep<KeyValueMapping[TKey]>;
                 };
       }[OnyxKey]
     | {
           [TKey in CollectionKeyBase]: {
               onyxMethod: typeof METHOD.MERGE_COLLECTION;
               key: TKey;
-              value: Record<`${TKey}${string}`, PartialDeep<KeyValueMapping[TKey]>>;
+              value: Record<`${TKey}${string}`, NullishDeep<KeyValueMapping[TKey]>>;
           };
       }[CollectionKeyBase];
 
@@ -244,10 +243,7 @@ declare function clear(keysToPreserve?: OnyxKey[]): Promise<void>;
  * @param collectionKey e.g. `ONYXKEYS.COLLECTION.REPORT`
  * @param collection Object collection keyed by individual collection member keys and values
  */
-declare function mergeCollection<TKey extends CollectionKeyBase, TMap>(
-    collectionKey: TKey,
-    collection: Collection<TKey, TMap, PartialDeep<KeyValueMapping[TKey]>>,
-): Promise<void>;
+declare function mergeCollection<TKey extends CollectionKeyBase, TMap>(collectionKey: TKey, collection: Collection<TKey, TMap, NullishDeep<KeyValueMapping[TKey]>>): Promise<void>;
 
 /**
  * Insert API responses and lifecycle data into Onyx
