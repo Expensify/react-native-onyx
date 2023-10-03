37580f31ebf1384204a9c51837f911f5a0fc9bff
Ana Margarida Silva <ana.margarida.silva@callstack.com>
2023-10-03 10:56:15 +0100

    test: comment out webkit from playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -42,10 +42,13 @@ module.exports = defineConfig({
             name: 'firefox',
             use: {...devices['Desktop Firefox']},
         },
+
+        /*
         {
             name: 'webkit',
             use: {...devices['Desktop Safari']},
         },
+        */
     ],
 
     /* Run your local dev server before starting the tests */
