1908c72ab5868f573d259be4214f4050535290bd
tienifr <christianwen18@gmail.com>
2023-10-19 11:41:08 +0700

    add comment

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,3 +1,10 @@
+/**
+ * Merges two objects and removes null values if "shouldRemoveNullObjectValues" is set to true
+ *
+ * We generally want to remove null values from objects written to disk and cache, because it decreases the amount of data stored in memory and on disk.
+ * On native, when merging an existing value with new changes, SQLite will use JSON_PATCH, which removes top-level nullish values.
+ * To be consistent with the behaviour for merge, we'll also want to remove null values for "set" operations.
+ */
 declare function fastMerge<T>(
     target: T,
     source: T,
