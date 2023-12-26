735ff1ef6d41cc60435c40c77bab33411639baaf
Christoph Pader <christoph@margelo.io>
2023-12-26 20:48:38 +0100

    move MultiStorage and replace mock

--- jestSetup.js
+++ jestSetup.js
@@ -1,7 +1,7 @@
-jest.mock('./lib/storage');
-jest.mock('./lib/storage/NativeStorage', () => require('./lib/storage/__mocks__'));
-jest.mock('./lib/storage/WebStorage', () => require('./lib/storage/__mocks__'));
-jest.mock('./lib/storage/providers/IDBKeyVal', () => require('./lib/storage/__mocks__'));
+jest.mock('./lib/MultiStorage');
+jest.mock('./lib/storage/NativeStorage', () => require('./lib/MultiStorage/__mocks__'));
+jest.mock('./lib/storage/WebStorage', () => require('./lib/MultiStorage/__mocks__'));
+jest.mock('./lib/storage/providers/IDBKeyVal', () => require('./lib/MultiStorage/__mocks__'));
 
 jest.mock('react-native-device-info', () => ({getFreeDiskStorage: () => {}}));
 jest.mock('react-native-quick-sqlite', () => ({
