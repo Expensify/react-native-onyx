4c1624bbe254c077e8fdbde50a10e6957d7175de
Fábio Henriques <contact@fabiohenriques.dev>
2024-01-25 12:11:10 +0000

    First test implementation of useOnyx hook

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,7 @@
 import Onyx, {OnyxUpdate, ConnectOptions} from './Onyx';
 import {CustomTypeOptions, OnyxCollection, OnyxEntry, NullishDeep, KeyValueMapping, OnyxKey, Selector} from './types';
 import withOnyx from './withOnyx';
+import {useOnyx, useOnyxWithSyncExternalStore} from './useOnyx';
 
 export default Onyx;
-export {CustomTypeOptions, OnyxCollection, OnyxEntry, OnyxUpdate, withOnyx, ConnectOptions, NullishDeep, KeyValueMapping, OnyxKey, Selector};
+export {CustomTypeOptions, OnyxCollection, OnyxEntry, OnyxUpdate, withOnyx, ConnectOptions, NullishDeep, KeyValueMapping, OnyxKey, Selector, useOnyx, useOnyxWithSyncExternalStore};
---
+++ lib/useOnyx.ts
@@ -0,0 +1,89 @@
+/* eslint-disable es/no-nullish-coalescing-operators */
+/* eslint-disable es/no-optional-chaining */
+import {useEffect, useRef, useState} from 'react';
+import Onyx from './Onyx';
+// eslint-disable-next-line rulesdir/prefer-import-module-contents
+import type {CollectionKeyBase, KeyValueMapping, OnyxCollection, OnyxEntry, OnyxKey} from './types';
+
+type OnyxValue<TKey extends OnyxKey> = TKey extends CollectionKeyBase ? OnyxCollection<KeyValueMapping[TKey]> : OnyxEntry<KeyValueMapping[TKey]>;
+
+type UseOnyxOptions<TKey extends OnyxKey> = {
+    /**
+     * Determines if this key in this subscription is safe to be evicted.
+     */
+    canEvict?: boolean;
+
+    /**
+     * If set to false, then no data will be prefilled into the component.
+     */
+    initWithStoredValues?: boolean;
+
+    /**
+     * TODO: Check if we still need this flag and associated logic.
+     */
+    allowStaleData?: boolean;
+
+    /**
+     * Sets an initial value to be returned by the hook during the first render.
+     */
+    initialValue?: OnyxValue<TKey>;
+};
+
+function useOnyx<TKey extends OnyxKey>(keyParam: TKey, options?: UseOnyxOptions<TKey>): OnyxValue<TKey> {
+    const [value, setValue] = useState<OnyxValue<TKey>>(options?.initialValue ?? (null as OnyxValue<TKey>));
+
+    /**
+     * Prevents key reassignment.
+     */
+    const keyRef = useRef(keyParam);
+    const key = keyRef.current;
+
+    const connectionIDRef = useRef<number | null>(null);
+
+    useEffect(() => {
+        // eslint-disable-next-line rulesdir/prefer-onyx-connect-in-libs
+        connectionIDRef.current = Onyx.connect({
+            key,
+            callback: (val) => {
+                setValue(val as OnyxValue<TKey>);
+            },
+            initWithStoredValues: options?.initWithStoredValues,
+        });
+
+        return () => {
+            if (!Onyx.isSafeEvictionKey(key)) {
+                throw new Error(`canEvict can't be used on key '${key}'. This key must explicitly be flagged as safe for removal by adding it to Onyx.init({safeEvictionKeys: []}).`);
+            }
+
+            if (!connectionIDRef.current) {
+                return;
+            }
+
+            Onyx.disconnect(connectionIDRef.current);
+        };
+    }, []);
+
+    /**
+     * Mimics withOnyx's checkEvictableKeys() behavior.
+     */
+    useEffect(() => {
+        if (options?.canEvict === undefined || !connectionIDRef.current) {
+            return;
+        }
+
+        if (options.canEvict) {
+            Onyx.removeFromEvictionBlockList(key, connectionIDRef.current);
+        } else {
+            Onyx.addToEvictionBlockList(key, connectionIDRef.current);
+        }
+    }, [options?.canEvict]);
+
+    return value;
+}
+
+function useOnyxWithSyncExternalStore<TKey extends OnyxKey>(_key: TKey, _options?: UseOnyxOptions<TKey>): OnyxValue<TKey> {
+    // @ts-expect-error TODO
+    return null;
+}
+
+export {useOnyx, useOnyxWithSyncExternalStore};
