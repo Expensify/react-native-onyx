8cca556b08bf858c17a7d4f5ddd2879ac3f456d4
Blazej Kustra <blazej.kustra@swmansion.com>
2024-02-07 11:58:18 +0100

    Migrate utils to TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,6 +53,8 @@ module.exports = {
                 '@typescript-eslint/array-type': ['error', {default: 'array-simple'}],
                 '@typescript-eslint/consistent-type-definitions': 'off',
                 'rulesdir/no-multiple-onyx-in-file': 'off',
+                'valid-jsdoc': 'off',
+                'rulesdir/prefer-import-module-contents': 'off',
             },
         },
         {
