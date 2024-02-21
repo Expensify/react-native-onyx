2dd23129f5cee6388f622a38a75eca39e1c918a1
Blazej Kustra <blazej.kustra@swmansion.com>
2024-02-21 17:56:27 +0100

    Migrate reamaining files to TS

--- lib/createDeferredTask.ts
+++ lib/createDeferredTask.ts
@@ -1,13 +1,16 @@
+type DeferredTask = {
+    promise: Promise<unknown>;
+    resolve: (value: unknown) => void;
+};
+
 /**
  * Create a deferred task that can be resolved when we call `resolve()`
  * The returned promise will complete when we call `resolve`
  * Useful when we want to wait for a tasks that is resolved from an external action
- *
- * @template T
- * @returns {{ resolve: function(*), promise: Promise<T|void> }}
  */
-export default function createDeferredTask() {
-    const deferred = {};
+export default function createDeferredTask(): DeferredTask {
+    const deferred: DeferredTask = {} as DeferredTask;
+
     deferred.promise = new Promise((res) => {
         deferred.resolve = res;
     });
