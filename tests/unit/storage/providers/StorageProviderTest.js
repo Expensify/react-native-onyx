b6fb255c936dc2a00f3a070e293df41ccc1fc1a0
kirillzyusko <kiryl@margelo.io>
2024-02-28 16:56:18 +0100

    test: verify all methods are implemented

--- tests/unit/storage/providers/StorageProviderTest.js
+++ tests/unit/storage/providers/StorageProviderTest.js
@@ -6,10 +6,12 @@ jest.unmock('../../../../lib/storage/providers/IDBKeyValProvider');
 import _ from 'underscore';
 import NativeStorage from '../../../../lib/storage/platforms/index.native';
 import WebStorage from '../../../../lib/storage/platforms/index';
+import MemoryOnlyProvider from '../../../../lib/storage/providers/MemoryOnlyProvider';
 
 it('storage providers have same methods implemented', () => {
+    const memoryOnlyMethods = _.keys(MemoryOnlyProvider);
     const nativeMethods = _.keys(NativeStorage);
     const webMethods = _.keys(WebStorage);
-    const unimplementedMethods = _.difference(nativeMethods, webMethods);
+    const unimplementedMethods = _.difference(nativeMethods, webMethods, memoryOnlyMethods);
     expect(unimplementedMethods.length).toBe(0);
 });
