2a9aa58e469d126abc351d4d0720f91ff868a8e4
Blazej Kustra <blazej.kustra@swmansion.com>
2024-02-28 17:52:35 +0100

    Remove unneccessary metrics tests

--- tests/unit/onyxMetricsDecorationTest.js
+++ tests/unit/onyxMetricsDecorationTest.js
@@ -15,20 +15,6 @@ describe('Onyx', () => {
             Onyx = require('../../lib').default;
         });
 
-        it('Should expose metrics methods when `captureMetrics` is true', () => {
-            // When Onyx is initialized with `captureMetrics: true`
-            Onyx.init({
-                keys: ONYX_KEYS,
-                registerStorageEventListener: jest.fn(),
-                captureMetrics: true,
-            });
-
-            // Then Onyx should have statistic related methods
-            expect(Onyx.getMetrics).toEqual(expect.any(Function));
-            expect(Onyx.printMetrics).toEqual(expect.any(Function));
-            expect(Onyx.resetMetrics).toEqual(expect.any(Function));
-        });
-
         it('Should not expose metrics methods when `captureMetrics` is false or not set', () => {
             // When Onyx is initialized without setting `captureMetrics`
             Onyx.init({
@@ -52,28 +38,6 @@ describe('Onyx', () => {
             expect(Onyx.getMetrics).not.toBeDefined();
             expect(Onyx.printMetrics).not.toBeDefined();
             expect(Onyx.resetMetrics).not.toBeDefined();
-        });
-
-        it('Should decorate exposed methods', () => {
-            // Given Onyx is initialized with `captureMetrics: true`
-            Onyx.init({
-                keys: ONYX_KEYS,
-                registerStorageEventListener: jest.fn(),
-                captureMetrics: true,
-            });
-
-            // When calling decorated methods through Onyx[methodName]
-            const methods = ['set', 'multiSet', 'clear', 'merge', 'mergeCollection'];
-            methods.forEach((name) => Onyx[name]('mockKey', {mockKey: {mockValue: 'mockValue'}}));
-
-            return waitForPromisesToResolve().then(() => {
-                // Then metrics should have captured data for each method
-                const summaries = Onyx.getMetrics().summaries;
-
-                methods.forEach((name) => {
-                    expect(summaries[`Onyx:${name}`].total).toBeGreaterThan(0);
-                });
-            });
         });
     });
 });
