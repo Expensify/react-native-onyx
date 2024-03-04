6236ca0615d7d20ba93b05a1584c77cef9bb98b5
Blazej Kustra <blazej.kustra@swmansion.com>
2024-03-04 18:33:04 +0100

    Migrate index.js

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Onyx, {OnyxUpdate, ConnectOptions} from './Onyx';
-import {CustomTypeOptions, OnyxCollection, OnyxEntry, NullishDeep, KeyValueMapping, OnyxKey, Selector, WithOnyxInstanceState} from './types';
-import withOnyx from './withOnyx';
-
-export default Onyx;
-export {CustomTypeOptions, OnyxCollection, OnyxEntry, OnyxUpdate, withOnyx, ConnectOptions, NullishDeep, KeyValueMapping, OnyxKey, Selector, WithOnyxInstanceState};
---
+++ lib/index.ts
@@ -0,0 +1,8 @@
+import Onyx from './Onyx';
+import type {OnyxUpdate, Mapping} from './Onyx';
+import type {CustomTypeOptions, OnyxCollection, OnyxEntry, NullishDeep, KeyValueMapping, OnyxKey, Selector, WithOnyxInstanceState} from './types';
+import withOnyx from './withOnyx';
+
+export default Onyx;
+export {withOnyx};
+export type {CustomTypeOptions, OnyxCollection, OnyxEntry, OnyxUpdate, Mapping as ConnectOptions, NullishDeep, KeyValueMapping, OnyxKey, Selector, WithOnyxInstanceState};
