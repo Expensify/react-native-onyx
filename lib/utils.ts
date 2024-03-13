b00e03611bbc85bdb610b4d02a8a6636d7e4cb02
Blazej Kustra <blazej.kustra@swmansion.com>
2024-03-13 14:44:16 +0100

    Adjust utils, provde better typings

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/prefer-for-of */
-import type {OnyxKey} from './types';
+import type {OnyxEntry, OnyxKey, OnyxValue} from './types';
 
 type EmptyObject = Record<string, never>;
 type EmptyValue = EmptyObject | null | undefined;
@@ -14,7 +14,7 @@ function isEmptyObject<T>(obj: T | EmptyValue): obj is EmptyValue {
 /**
  * Checks whether the given value can be merged. It has to be an object, but not an array, RegExp or Date.
  */
-function isMergeableObject(value: unknown): boolean {
+function isMergeableObject(value: unknown): value is Record<string, unknown> {
     const nonNullObject = value != null ? typeof value === 'object' : false;
     return nonNullObject && Object.prototype.toString.call(value) !== '[object RegExp]' && Object.prototype.toString.call(value) !== '[object Date]' && !Array.isArray(value);
 }
@@ -26,15 +26,15 @@ function isMergeableObject(value: unknown): boolean {
  * @param shouldRemoveNullObjectValues - If true, null object values will be removed.
  * @returns - The merged object.
  */
-function mergeObject<TTarget extends unknown[] | Record<string, unknown>>(target: TTarget, source: TTarget, shouldRemoveNullObjectValues = true): TTarget {
+function mergeObject<TValue extends OnyxValue<OnyxKey>>(target: OnyxEntry<TValue>, source: TValue, shouldRemoveNullObjectValues = true): TValue {
     const destination: Record<string, unknown> = {};
     if (isMergeableObject(target)) {
         // lodash adds a small overhead so we don't use it here
         const targetKeys = Object.keys(target);
         for (let i = 0; i < targetKeys.length; ++i) {
             const key = targetKeys[i];
-            const sourceValue = source?.[key as keyof TTarget];
-            const targetValue = target?.[key as keyof TTarget];
+            const sourceValue = source?.[key];
+            const targetValue = target?.[key];
 
             // If shouldRemoveNullObjectValues is true, we want to remove null values from the merged object
             const isSourceOrTargetNull = targetValue === null || sourceValue === null;
@@ -46,11 +46,11 @@ function mergeObject<TTarget extends unknown[] | Record<string, unknown>>(target
         }
     }
 
-    const sourceKeys = Object.keys(source);
+    const sourceKeys = Object.keys(source!);
     for (let i = 0; i < sourceKeys.length; ++i) {
         const key = sourceKeys[i];
-        const sourceValue = source?.[key as keyof TTarget];
-        const targetValue = target?.[key as keyof TTarget];
+        const sourceValue = source?.[key];
+        const targetValue = target?.[key];
 
         // If shouldRemoveNullObjectValues is true, we want to remove null values from the merged object
         const shouldOmitSourceKey = shouldRemoveNullObjectValues && sourceValue === null;
@@ -63,14 +63,14 @@ function mergeObject<TTarget extends unknown[] | Record<string, unknown>>(target
 
             if (isSourceKeyMergable && targetValue) {
                 // eslint-disable-next-line no-use-before-define
-                destination[key] = fastMerge(targetValue, sourceValue as typeof targetValue, shouldRemoveNullObjectValues);
+                destination[key] = fastMerge(targetValue as OnyxValue<OnyxKey>, sourceValue, shouldRemoveNullObjectValues);
             } else if (!shouldRemoveNullObjectValues || sourceValue !== null) {
                 destination[key] = sourceValue;
             }
         }
     }
 
-    return destination as TTarget;
+    return destination as TValue;
 }
 
 /**
@@ -80,7 +80,7 @@ function mergeObject<TTarget extends unknown[] | Record<string, unknown>>(target
  * On native, when merging an existing value with new changes, SQLite will use JSON_PATCH, which removes top-level nullish values.
  * To be consistent with the behaviour for merge, we'll also want to remove null values for "set" operations.
  */
-function fastMerge<TTarget extends unknown[] | Record<string, unknown>>(target: TTarget, source: TTarget, shouldRemoveNullObjectValues = true): TTarget {
+function fastMerge<TValue extends OnyxValue<OnyxKey>>(target: TValue, source: TValue, shouldRemoveNullObjectValues = true): TValue {
     // We have to ignore arrays and nullish values here,
     // otherwise "mergeObject" will throw an error,
     // because it expects an object as "source"
