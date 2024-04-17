9e36847fca1678bd79e685b307e99d9433abbf15
Fábio Henriques <contact@fabiohenriques.dev>
2024-04-17 19:12:31 +0100

    Fix prettier

--- lib/withOnyx.d.ts
+++ lib/withOnyx.d.ts
@@ -126,7 +126,7 @@ type OnyxPropCollectionMapping<TComponentProps, TOnyxProps, TOnyxProp extends ke
 
 /**
  * @deprecated Use `useOnyx` instead of `withOnyx` whenever possible.
- * 
+ *
  * This is a higher order component that provides the ability to map a state property directly to
  * something in Onyx (a key/value store). That way, as soon as data in Onyx changes, the state will be set and the view
  * will automatically change to reflect the new data.
