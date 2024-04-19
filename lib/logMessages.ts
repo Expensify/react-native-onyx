e81e210966aa9aab76fde4b3b3f073a16f31c986
dominictb <dominic-tb@outlook.com>
2024-04-19 09:18:58 +0700

    fix: small update on log message
    Signed-off-by: dominictb <tb-dominic@outlook.com>

--- lib/logMessages.ts
+++ lib/logMessages.ts
@@ -1,6 +1,6 @@
 const logMessages = {
     incompatibleUpdateAlert: (key: string, operation: string, existingValueType?: string, newValueType?: string) => {
-        return `Warning: Trying to apply "${operation}" with ${newValueType ?? 'unknown'} type to ${existingValueType ?? 'unknown'} type in the key ${key}`;
+        return `Warning: Trying to apply "${operation}" with ${newValueType ?? 'unknown'} type to ${existingValueType ?? 'unknown'} type in the key "${key}"`;
     },
 };
 
