a6f3e05e113984cbf76409dd3ca9761697bae0e5
Fábio Henriques <contact@fabiohenriques.dev>
2024-05-16 18:55:10 +0100

    Moves react.d.ts inside lib and excludes it from dist

--- lib/withOnyx.tsx
+++ lib/withOnyx.tsx
@@ -217,6 +217,13 @@ function mapOnyxToStateEntries<TComponentProps, TOnyxProps>(mapOnyxToState: MapO
     return Object.entries(mapOnyxToState) as Array<[keyof TOnyxProps, WithOnyxMapping<TComponentProps, TOnyxProps>]>;
 }
 
+/**
+ * @deprecated Use `useOnyx` instead of `withOnyx` whenever possible.
+ *
+ * This is a higher order component that provides the ability to map a state property directly to
+ * something in Onyx (a key/value store). That way, as soon as data in Onyx changes, the state will be set and the view
+ * will automatically change to reflect the new data.
+ */
 export default function <TComponentProps, TOnyxProps>(
     mapOnyxToState: MapOnyxToState<TComponentProps, TOnyxProps>,
     shouldDelayUpdates = false,
