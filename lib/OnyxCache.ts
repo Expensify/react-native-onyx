26e2af4fd6be1596254c0a81a38900f5834bd9a4
Christoph Pader <chris@margelo.io>
2024-05-29 17:29:01 +0200

    remove line (re-trigger tests)

--- lib/OnyxCache.ts
+++ lib/OnyxCache.ts
@@ -2,7 +2,6 @@ import {deepEqual} from 'fast-equals';
 import bindAll from 'lodash/bindAll';
 import utils from './utils';
 import type {OnyxKey, OnyxValue} from './types';
-
 /**
  * In memory cache providing data by reference
  * Encapsulates Onyx cache related functionality
