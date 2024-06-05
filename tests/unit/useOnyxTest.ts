4b1dccc8d14366c53448a2b2e6127ffaa97837b2
Christoph Pader <chris@margelo.io>
2024-06-05 17:05:48 +0200

    remove unused ts-expect-error pragmas

--- tests/unit/useOnyxTest.ts
+++ tests/unit/useOnyxTest.ts
@@ -163,7 +163,6 @@ describe('useOnyx', () => {
         });
 
         it('should return selected data from a collection key', async () => {
-            // @ts-expect-error bypass
             Onyx.mergeCollection(ONYXKEYS.COLLECTION.TEST_KEY, {
                 [`${ONYXKEYS.COLLECTION.TEST_KEY}entry1`]: {id: 'entry1_id', name: 'entry1_name'},
                 [`${ONYXKEYS.COLLECTION.TEST_KEY}entry2`]: {id: 'entry2_id', name: 'entry2_name'},
@@ -196,7 +195,6 @@ describe('useOnyx', () => {
         });
 
         it('should not change selected data if a property outside the selector was changed', async () => {
-            // @ts-expect-error bypass
             Onyx.mergeCollection(ONYXKEYS.COLLECTION.TEST_KEY, {
                 [`${ONYXKEYS.COLLECTION.TEST_KEY}entry1`]: {id: 'entry1_id', name: 'entry1_name'},
                 [`${ONYXKEYS.COLLECTION.TEST_KEY}entry2`]: {id: 'entry2_id', name: 'entry2_name'},
