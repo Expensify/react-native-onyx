e345a47fb677e4928d922c0cb2b025202258d19c
Fábio Henriques <contact@fabiohenriques.dev>
2025-04-13 21:34:35 +0100

    Improve buildActions.sh script

--- .github/scripts/buildActions.sh
+++ .github/scripts/buildActions.sh
@@ -4,40 +4,39 @@
 # This bundles them with their dependencies into a single executable node.js script.
 
 # In order for this script to be safely run from anywhere, we cannot use the raw relative path '../actions'.
-declare ACTIONS_DIR
-ACTIONS_DIR="$(dirname "$(dirname "$0")")/actions/javascript"
+ACTIONS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/../actions/javascript" &>/dev/null && pwd)"
+readonly ACTIONS_DIR
 
-# List of paths to all JS files that implement our GH Actions
-declare -r GITHUB_ACTIONS=(
-    "$ACTIONS_DIR/validateReassureOutput/validateReassureOutput.ts"
-)
+# This stores all the process IDs of the ncc commands so they can run in parallel
+ASYNC_BUILDS=()
 
-# This will be inserted at the top of all compiled files as a warning to devs.
-declare -r NOTE_DONT_EDIT='/**
+# Build all the actions in the background
+for ACTION in "$ACTIONS_DIR"/*/*.ts; do
+    ACTION_DIR=$(dirname "$ACTION")
+    npx ncc build --transpile-only --external encoding "$ACTION" -o "$ACTION_DIR" &
+    ASYNC_BUILDS+=($!)
+done
+
+# Wait for the background build to finish
+EXIT_CODE=0
+for PID in "${ASYNC_BUILDS[@]}"; do
+    if ! wait "$PID"; then
+        EXIT_CODE=1
+    fi
+done
+
+# Prepend this note at the top of all compiled files as a warning to devs.
+readonly NOTE_DONT_EDIT='/**
  * NOTE: This is a compiled file. DO NOT directly edit this file.
  */
 '
-
-# This stores all the process IDs of the ncc commands so they can run in parallel
-declare ASYNC_BUILDS
-
-for ((i=0; i < ${#GITHUB_ACTIONS[@]}; i++)); do
-  ACTION=${GITHUB_ACTIONS[$i]}
-  ACTION_DIR=$(dirname "$ACTION")
-
-  # Build the action in the background
-  npx ncc build --transpile-only --external encoding "$ACTION" -o "$ACTION_DIR" &
-  ASYNC_BUILDS[i]=$!
+for OUTPUT_FILE in "$ACTIONS_DIR"/*/index.js; do
+    echo "$NOTE_DONT_EDIT$(cat "$OUTPUT_FILE")" > "$OUTPUT_FILE"
 done
 
-for ((i=0; i < ${#GITHUB_ACTIONS[@]}; i++)); do
-  ACTION=${GITHUB_ACTIONS[$i]}
-  ACTION_DIR=$(dirname "$ACTION")
+if [[ EXIT_CODE -ne 0 ]]; then
+    echo "❌ One or more builds failed"
+    exit 1
+fi
 
-  # Wait for the background build to finish
-  wait "${ASYNC_BUILDS[$i]}"
-
-  # Prepend the warning note to the top of the compiled file
-  OUTPUT_FILE="$ACTION_DIR/index.js"
-  echo "$NOTE_DONT_EDIT$(cat "$OUTPUT_FILE")" > "$OUTPUT_FILE"
-done
+echo "✅ All builds succeeded"
