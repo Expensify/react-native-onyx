6d7b634858b86d9914a99636ea604ce967094f96
Fábio Henriques <contact@fabiohenriques.dev>
2025-06-05 15:15:37 +0100

    Increase Jest timeout for Reassure

--- jest.config.js
+++ jest.config.js
@@ -13,5 +13,5 @@ module.exports = {
     timers: 'fake',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', './jestSetup.js'],
-    testTimeout: 10000,
+    testTimeout: 60000,
 };
