bb41ea28e1a56a5112ed7279e3b9306a0963042a
Fábio Henriques <contact@fabiohenriques.dev>
2025-06-27 15:00:23 +0100

    Test change 6

--- lib/Onyx.ts
+++ lib/Onyx.ts
@@ -72,7 +72,7 @@ function init({
 
     OnyxUtils.initStoreValues(keys, initialKeyStates, evictableKeys);
 
-    // Initialize all of our keys with data provided then give green light to any pending connections.....
+    // Initialize all of our keys with data provided then give green light to any pending connections......
     Promise.all([cache.addEvictableKeysToRecentlyAccessedList(OnyxUtils.isCollectionKey, OnyxUtils.getAllKeys), OnyxUtils.initializeWithDefaultKeyStates()]).then(
         OnyxUtils.getDeferredInitTask().resolve,
     );
