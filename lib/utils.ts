bd1cf4bea7023250a2febf58643e08c5b51c089d
Fábio Henriques <contact@fabiohenriques.dev>
2025-07-02 18:25:28 +0100

    Refactor removeNestedNullValues to be performant

--- lib/utils.ts
+++ lib/utils.ts
@@ -106,12 +106,34 @@ function fastMerge<TValue>(target: TValue, source: TValue, shouldRemoveNestedNul
 
 /** Deep removes the nested null values from the given value. */
 function removeNestedNullValues<TValue extends OnyxInput<OnyxKey> | null>(value: TValue): TValue {
-    if (typeof value === 'object' && !Array.isArray(value)) {
-        const objectValue = value as Record<string, unknown>;
-        return fastMerge(objectValue, objectValue) as TValue;
+    if (value === null || value === undefined) {
+        return value;
     }
 
-    return value;
+    if (typeof value !== 'object' || Array.isArray(value)) {
+        return value;
+    }
+
+    const result: Record<string, unknown> = {};
+
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const key in value) {
+        const propertyValue = value[key];
+
+        if (propertyValue === null || propertyValue === undefined) {
+            // eslint-disable-next-line no-continue
+            continue;
+        }
+
+        if (typeof propertyValue === 'object' && !Array.isArray(propertyValue)) {
+            const valueWithoutNestedNulls = removeNestedNullValues(propertyValue);
+            result[key] = valueWithoutNestedNulls;
+        } else {
+            result[key] = propertyValue;
+        }
+    }
+
+    return result as TValue;
 }
 
 /** Formats the action name by uppercasing and adding the key if provided. */
