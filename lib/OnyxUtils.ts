75a6fc349a6191cfe38f79879950837d9243b84e
Antony Kithinzi <akithinzi@icloud.com>
2025-07-11 01:50:22 +0300

    keysChanged setting lastConnectionData to new cached value

--- lib/OnyxUtils.ts
+++ lib/OnyxUtils.ts
@@ -695,6 +695,7 @@ function keysChanged<TKey extends CollectionKeyBase>(
 
                 const subscriberCallback = subscriber.callback as DefaultConnectCallback<TKey>;
                 subscriberCallback(cachedCollection[subscriber.key], subscriber.key as TKey);
+		lastConnectionData.set(subscriber,key, cachedCollection[subscriber.key]);
                 continue;
             }
 
