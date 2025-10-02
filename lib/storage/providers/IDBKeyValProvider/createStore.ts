0680f6cc8951e67540bc3c0bd42eda723cc9d141
Blazej Kustra <blazej.kustra@swmansion.com>
2025-10-02 14:08:08 +0200

    Refactor createStore function in IDBKeyValProvider

--- lib/storage/providers/IDBKeyValProvider/createStore.ts
+++ lib/storage/providers/IDBKeyValProvider/createStore.ts
@@ -2,32 +2,34 @@ import {promisifyRequest} from 'idb-keyval';
 import type {UseStore} from 'idb-keyval';
 import {logInfo} from '../../../Logger';
 
-// This is a copy of the createStore function from idb-keyval, but we need to use it to create the store manually.
-// We do this because we need to create the database manually in order to ensure that the store exists before we use it.
+// This is a copy of the createStore function from idb-keyval, we need a custom implementation
+// because we need to create the database manually in order to ensure that the store exists before we use it.
 // If the store does not exist, idb-keyval will throw an error
 // source: https://github.com/jakearchibald/idb-keyval/blob/9d19315b4a83897df1e0193dccdc29f78466a0f3/src/index.ts#L12
 function createStore(dbName: string, storeName: string): UseStore {
-    let database: Promise<IDBDatabase> | undefined;
+    let dbp: Promise<IDBDatabase> | undefined;
     const getDB = () => {
-        if (database) return database;
+        if (dbp) return dbp;
         const request = indexedDB.open(dbName);
         request.onupgradeneeded = () => request.result.createObjectStore(storeName);
-        database = promisifyRequest(request);
-        database.then(
+        dbp = promisifyRequest(request);
+
+        dbp.then(
             (db) => {
                 // It seems like Safari sometimes likes to just close the connection.
                 // It's supposed to fire this event when that happens. Let's hope it does!
                 // eslint-disable-next-line no-param-reassign
-                db.onclose = () => (database = undefined);
+                db.onclose = () => (dbp = undefined);
             },
             // eslint-disable-next-line @typescript-eslint/no-empty-function
             () => {},
         );
-
-        return database;
+        return dbp;
     };
 
-    const fixStore = (db: IDBDatabase) => {
+    // Ensures the store exists in the DB. If missing, bumps the version to trigger
+    // onupgradeneeded, recreates the store, and returns a promise to the new DB.
+    const verifyStoreExists = (db: IDBDatabase) => {
         if (db.objectStoreNames.contains(storeName)) {
             return db;
         }
@@ -47,13 +49,13 @@ function createStore(dbName: string, storeName: string): UseStore {
             updatedDatabase.createObjectStore(storeName);
         };
 
-        database = promisifyRequest(request);
-        return database;
+        dbp = promisifyRequest(request);
+        return dbp;
     };
 
     return (txMode, callback) =>
         getDB()
-            .then(fixStore)
+            .then(verifyStoreExists)
             .then((db) => callback(db.transaction(storeName, txMode).objectStore(storeName)));
 }
 
