2b83ba7e057a6a2c1b77b1504be90d9fa7e3b844
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2025-10-17 15:22:20 +0200

    Revert "implement batch in partialSetCollection"
    This reverts commit 8c6dcf2f321601979788ff64a45472ea6d40b0fe.

--- lib/OnyxUtils.ts
+++ lib/OnyxUtils.ts
@@ -1400,18 +1400,13 @@ function partialSetCollection<TKey extends CollectionKeyBase, TMap>(collectionKe
         const previousCollection = getCachedCollection(collectionKey, existingKeys);
         const keyValuePairs = prepareKeyValuePairsForStorage(mutableCollection, true);
 
-        setIsProcessingCollectionUpdate(true);
         keyValuePairs.forEach(([key, value]) => cache.set(key, value));
 
         const updatePromise = scheduleNotifyCollectionSubscribers(collectionKey, mutableCollection, previousCollection);
 
         return Storage.multiSet(keyValuePairs)
-            .catch((error) => {
-                setIsProcessingCollectionUpdate(false);
-                return evictStorageAndRetry(error, partialSetCollection, collectionKey, collection);
-            })
+            .catch((error) => evictStorageAndRetry(error, partialSetCollection, collectionKey, collection))
             .then(() => {
-                setIsProcessingCollectionUpdate(false);
                 sendActionToDevTools(METHOD.SET_COLLECTION, undefined, mutableCollection);
                 return updatePromise;
             });
