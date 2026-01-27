488f6359d2630dc99dd61b6ec94210a9203c9282
Fábio Henriques <contact@fabiohenriques.dev>
2026-01-27 13:34:36 +0000

    Test change 1

--- lib/Onyx.ts
+++ lib/Onyx.ts
@@ -402,7 +402,7 @@ function clear(keysToPreserve: OnyxKey[] = []): Promise<void> {
 }
 
 /**
- * Insert API responses and lifecycle data into Onyx
+ * Insert API responses and lifecycle data into Onyx.
  *
  * @param data An array of objects with update expressions
  * @returns resolves when all operations are complete
