8ae51453a935ed95eb122c575ceaa5d9f0ff7e6f
Michał Muzyk <mimuzyk@gmail.com>
2026-03-02 15:14:43 +0100

    test - should fail (not transpiled)

--- .github/actions/javascript/validatePRChecklist/validatePRChecklist.ts
+++ .github/actions/javascript/validatePRChecklist/validatePRChecklist.ts
@@ -18,6 +18,7 @@ const UNCHECKED_PATTERN = /- \[ \]/g;
 const CHECKED_PATTERN = /- \[x\]/gi;
 
 function getAuthorChecklistSection(body: string): string {
+    const b = 'b';
     const startMarker = '### Author Checklist';
     const endMarker = '### Screenshots/Videos';
     const startIndex = body.indexOf(startMarker);
